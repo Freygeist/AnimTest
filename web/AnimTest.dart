bef8cc9d87759bcd8d76523fd191de0cd288827c
Freygeist <freygeist@github.not>
2013-01-25 18:33:13 +0100

    added assetManager and sprites

--- web/AnimTest.dart
+++ web/AnimTest.dart
@@ -1,5 +1,24 @@
-library AnimTest;
+library bad_aliens;
+
+import 'dart:html';
+import 'dart:math' as Math;
+
+part 'AssetManager.dart';
+
 
 void main(){
+  CanvasElement canvas = query('#canvas');
+  var ctx = canvas.context2d;
+
+
+  //assetmanager für bilder
+  var assetManager = new AssetManager();
+  assetManager.queueDownload('img/left_sprite.png');
+
+
+  //aufbauen einer neuen gameEntity
+  //initalisieren des spiels
+  //starten des spiel loops
+
 
 }
---
+++ web/AssetManager.dart
@@ -0,0 +1,46 @@
+part of bad_aliens;
+
+class AssetManager {
+  int successCount = 0;
+  int errorCount = 0;
+  Map cache = {};
+  List downloadQueue = [];
+
+  void queueDownload(String path) {
+    downloadQueue.add(path);
+  }
+
+  void downloadAll(downloadCallback) {
+    if (downloadQueue.length == 0) {
+      downloadCallback();
+    }
+
+    for (final path in downloadQueue) {
+      var img = new ImageElement();
+      img.onLoad.listen((event) {
+        print('${img.src} is loaded');
+        successCount += 1;
+        if (isDone()) {
+            downloadCallback();
+        }
+      });
+      img.onError.listen((event) {
+        errorCount += 1;
+        if (isDone()) {
+            downloadCallback();
+        }
+      });
+      img.src = path;
+      cache[path] = img;
+    }
+  }
+
+  getAsset(path) {
+    return cache[path];
+  }
+
+  bool isDone() {
+    return (downloadQueue.length == successCount + errorCount);
+  }
+
+}
